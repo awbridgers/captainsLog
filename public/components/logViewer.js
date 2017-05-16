7bb81ebea1d7ad02e44fc095cefdeb2068e3ccbc
awbridgers <adamwbridgers@gmail.com>
2017-05-16 00:57:57 -0400

    update view logs page to display the correct date and respond to the next and previous buttons

--- public/components/logViewer.js
+++ public/components/logViewer.js
@@ -15,16 +15,25 @@ var divStyle = {
 var nextButton = {
   position:"relative", right: "-350px", bottom:"-52px",
   borderRadius:"2px",height: "35px", width: "50px",
-  fontSize: "16", textAlign:"center",
+  fontSize: 16, textAlign:"center",
 
 };
 
 var prevButton = {
   position: "relative", left: "-350px", top: "-54px",
   borderRadius:"2px",height: "35px", width: "50px",
-  fontSize: "16", textAlign:"center",
+  fontSize: 16, textAlign:"center",
 
 };
+//class constructor
+function logEntry (date, text){
+    this.date = date;
+    this.text = text;
+    this.type = "main";
+};
+
+
+
 
 
 
@@ -32,13 +41,79 @@ var prevButton = {
 export class Viewer extends React.Component{
   constructor(){
     super();
+    this.state = {date: "Date", logText: "", index: 0, logArray: [], loading: true};
+    this.previousButtonClick = this.previousButtonClick.bind(this);
+    this.nextButtonClick = this.nextButtonClick.bind(this);
   }
+  componentWillMount(){
+    this.uid = firebase.auth().currentUser.uid;
+    this.ref = firebase.database().ref(this.uid);
+    this.ref.once("value")
+      .then(function(snapshot) {
+        snapshot.forEach(function(childSnapshot) {
+          var temp = new logEntry(childSnapshot.val().date, childSnapshot.val().logEntry);
+          console.log(temp.text);
+          console.log(temp.type);
+
+
+
+
+          // if(this.state.logArray.length > 0){
+          //     if(this.state.logArray[0].date === temp.date){
+          //         temp.type = "supplemental"
+          //         var supLog = "<br /><b>Captain's Log Supplemental: </b>" + temp.text;
+          //         this.state.logArray[0].text += supLog;
+          //     }
+          //   }
+
+          if(temp.type === 'main'){
+            var tempArray = this.state.logArray.slice()
+            tempArray.unshift(temp);
+            this.setState({logArray: tempArray});
+            this.setState({loading: false});
+            //console.log(this.state.logArray[this.state.index].date);
+
+          }
+
+        }.bind(this));
+      }.bind(this));
+
+  }
+  previousButtonClick(){
+    if(this.state.index > 0){
+      this.setState({index: this.state.index - 1});
+    }
+    else{
+      console.log("this is the first entry");
+    }
+  }
+
+  nextButtonClick(){
+    if(this.state.index < this.state.logArray.length - 1){
+      this.setState({index: this.state.index + 1 });
+    }
+    else{
+      console.log("This is the last entry");
+    }
+  }
+
   render(){
+    //if no data has loaded into the array yet, just print the page elements with no info
+    if(this.state.loading){
+      return(
+        <div style = {{textAlign: "center", marginLeft: "auto", marginRight: "auto"}}>
+          <span><button style = {nextButton} type ="button">></button>
+          <h2><font color = "white"></font></h2>
+          <button style = {prevButton} type = "button">&lt;</button>
+          </span>
+        </div>
+      )
+    }
     return (
       <div style = {{textAlign: "center", marginLeft: "auto", marginRight: "auto"}}>
-        <span><button style = {nextButton} type ="button">></button>
-        <h2><font color = "white">DateDateDateDate</font></h2>
-        <button style = {prevButton} type = "button">&lt;</button>
+        <span><button style = {nextButton} type ="button" onClick = {this.nextButtonClick}>></button>
+        <h2><font color = "white">{this.state.logArray[this.state.index].date}</font></h2>
+        <button style = {prevButton} type = "button" onClick = {this.previousButtonClick}>&lt;</button>
         </span>
       </div>
   )}
